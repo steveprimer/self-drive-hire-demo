0b6373773540ab046745ecedaca73e7fd3f217d3
Anson Stephan <ansonstephan@gmail.com>
2025-07-26 02:15:40 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Hero from "./components/Hero";
 import CarGrid from "./components/CarGrid";
 import FAQ from "./components/FAQ";
 import CTASection from "./components/CTA";
-import ChatBot from "./components/ChatBot";
+import ChatBot from "./components/Chatbot";
 
 export default function App() {
   return (
